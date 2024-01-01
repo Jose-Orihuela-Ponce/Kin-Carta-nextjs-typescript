be5c4dd5aa587f6a53c0d1155ab083243ba95493
Jose Orihuela <freud_camus@hotmail.com>
2023-12-31 21:40:47 -0500

    modifing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
     <html lang="en">
       <body className="grid min-h-screen grid-rows-[auto,1fr,auto] px-4 container m-auto">
         <header className="leading-[60px] font-bold text-center text-2xl">
-          Case of Studies challenge
+          <h1>Case of Studies challenge</h1>
+          <p className="text-xl font-light">
+            Down here you can see some case of studies
+          </p>
         </header>
         <main className="py-12">{children}</main>
         <footer className="text-center leading-[60px] opacity-75">
